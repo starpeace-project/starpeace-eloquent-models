fc8b14d6824cdc37de45660ee10551c7195e8c49
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2018-08-19 23:55:19 +0100

    Update accessors

--- src/Eloquent/Common/World.php
+++ src/Eloquent/Common/World.php
@@ -13,4 +13,14 @@ class World extends BaseModel
     protected $dates = [ 'created_at', 'updated_at' ];
 
     protected $dateFormat ='Y-m-d H:i:s';
+
+    public function getCreatedAtAttribute($value)
+    {
+        return $value->format('d.m.Y H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return $value->format('d.m.Y H:i:s');
+    }
 }
