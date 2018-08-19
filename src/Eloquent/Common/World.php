b7f5d823331cea1d3ca4b02b7083e280f45ed301
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2018-08-19 19:34:31 +0100

    World Model

---
+++ src/Eloquent/Common/World.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Starpeace\Models\Eloquent\Common;
+
+use Starpeace\Models\Eloquent\BaseModel;
+
+class World extends BaseModel
+{
+    protected $table = 'worlds';
+
+    protected $fillable = [ 'galaxy_id', 'map_id', 'name', 'display_name', 'use_whitelist', 'use_blacklist' ];
+
+    protected $dates = [ 'created_at', 'updated_at' ];
+}
