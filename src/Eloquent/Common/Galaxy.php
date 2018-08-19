fe17b3562f201f44bd0d32d8ec2c1aedfea631e0
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2018-08-19 16:57:51 +0100

    Galaxy Model

---
+++ src/Eloquent/Common/Galaxy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Starpeace\Models\Eloquent\Common;
+
+use Starpeace\Models\Eloquent\BaseModel;
+
+class Galaxy extends BaseModel
+{
+    protected $table = 'galaxies';
+
+    protected $fillable = [ 'galaxy_type_id', 'galaxy_owner_id', 'name', 'display_name'];
+
+    protected $dates = [ 'created_at', 'updated_at'];
+
+
+}
