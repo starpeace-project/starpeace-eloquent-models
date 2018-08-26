383e37bd8716ab3bd86817b079e8e2351e438389
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2018-08-27 00:45:35 +0100

    Fix MapTown table name

--- src/Eloquent/Common/MapTown.php
+++ src/Eloquent/Common/MapTown.php
@@ -6,7 +6,7 @@ use Starpeace\Models\Eloquent\BaseModel;
 
 class MapTown extends BaseModel
 {
-    protected $table = 'maps_towns';
+    protected $table = 'map_towns';
 
     protected $fillable = ['map_id', 'faction_id', 'name', 'location_x', 'location_y'];
 
